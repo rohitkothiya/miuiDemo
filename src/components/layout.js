5978b7f49c8ed01954f9e7ddfb7455d94b6fab3b
rohitkothiya <rohit.kothiya2012@gmail.com>
2020-02-05 18:34:29 +0530

    logout added

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,101 +1,102 @@
-import React from "react"
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/ListAlt';
-import MailIcon from '@material-ui/icons/ViewList';
+import React from "react";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/ListAlt";
+import MailIcon from "@material-ui/icons/ViewList";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { Button } from "@material-ui/core";
+import axios from "axios";
 const drawerWidth = 240;
 
 const useStyles = makeStyles(theme => ({
   root: {
-    display: 'flex',
+    display: "flex"
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
+      duration: theme.transitions.duration.leavingScreen
+    })
   },
   appBarShift: {
     marginLeft: drawerWidth,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   menuButton: {
-    marginRight: 36,
+    marginRight: 36
   },
   hide: {
-    display: 'none',
+    display: "none"
   },
   drawer: {
     width: drawerWidth,
     flexShrink: 0,
-    whiteSpace: 'nowrap',
+    whiteSpace: "nowrap"
   },
   drawerOpen: {
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
+      duration: theme.transitions.duration.enteringScreen
+    })
   },
   drawerClose: {
-    transition: theme.transitions.create('width', {
+    transition: theme.transitions.create("width", {
       easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
+      duration: theme.transitions.duration.leavingScreen
     }),
-    overflowX: 'hidden',
+    overflowX: "hidden",
     width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
+    [theme.breakpoints.up("sm")]: {
+      width: theme.spacing(9) + 1
+    }
   },
   toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
     padding: theme.spacing(0, 1),
-    ...theme.mixins.toolbar,
+    ...theme.mixins.toolbar
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
-  },
+    padding: theme.spacing(3)
+  }
 }));
 
 const navigationList = [
   {
-    title : "Users",
-    path : "/users",
-    icon  : "ListAlt"
+    title: "Users",
+    path: "/users",
+    icon: "ListAlt"
   },
 
   {
-    title : "Posts",
-    path : "/posts",
-    icon  : "ListAlt"
+    title: "Posts",
+    path: "/posts",
+    icon: "ListAlt"
   }
-]
+];
 
 export default function MinidDrawer(props) {
   const classes = useStyles();
@@ -110,17 +111,34 @@ export default function MinidDrawer(props) {
   const handleDrawerClose = () => {
     setOpen(false);
   };
-const  handleLogout = () =>  {
-  localStorage.removeItem("userToken")
-  history.push("/");
-  }
+  const handleLogout = () => {
+    let data = localStorage.getItem("userToken");
+
+    let headers = {
+      headers: {
+        Authorization: `bearer ${data}`
+      }
+    };
+
+    axios
+      .get(`http://localhost:3000/logout`, headers)
+      .then(res => {
+        localStorage.removeItem("userToken");
+        history.push("/");
+      })
+      .catch(error => {
+        // alert("campaign. get assign camp- Something went wrong.");
+        console.log("erre", error);
+      });
+  };
+
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
         position="fixed"
         className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
+          [classes.appBarShift]: open
         })}
       >
         <Toolbar>
@@ -130,59 +148,72 @@ const  handleLogout = () =>  {
             onClick={handleDrawerOpen}
             edge="start"
             className={clsx(classes.menuButton, {
-              [classes.hide]: open,
+              [classes.hide]: open
             })}
           >
             <MenuIcon />
           </IconButton>
-          <div style={{display:"flex",justifyContent:"space-between",width:"100%"}}>
-          <Typography variant="h6" noWrap>
-            Demo App
-          </Typography>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              width: "100%"
+            }}
+          >
+            <Typography variant="h6" noWrap>
+              Demo App
+            </Typography>
 
-          <Button color="secondary" onClick={handleLogout}>
-            Logout
-          </Button>
+            <Button
+              color="secondary"
+              style={{ color: "white" }}
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
           </div>
-       
         </Toolbar>
       </AppBar>
       <Drawer
         variant="permanent"
         className={clsx(classes.drawer, {
           [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
+          [classes.drawerClose]: !open
         })}
         classes={{
           paper: clsx({
             [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
+            [classes.drawerClose]: !open
+          })
         }}
       >
         <div className={classes.toolbar}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
         <List>
           {navigationList.map((nav, index) => (
             <Link to={nav.path} key={index}>
-           
-            <ListItem button >
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={nav.title} />
-            </ListItem>
+              <ListItem button>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={nav.title} />
+              </ListItem>
             </Link>
           ))}
         </List>
         <Divider />
-       
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-          {props.children}
+        {props.children}
       </main>
     </div>
   );
--- src/components/users.js
+++ src/components/users.js
@@ -1,117 +1,120 @@
-import React,{useEffect, useState} from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Layout from "./layout"
+import React, { useEffect, useState } from "react";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Layout from "./layout";
 import axios from "axios";
 
 import { useHistory } from "react-router-dom";
-
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 const StyledTableCell = withStyles(theme => ({
   head: {
     backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
+    color: theme.palette.common.white
   },
   body: {
-    fontSize: 14,
-  },
+    fontSize: 14
+  }
 }))(TableCell);
 
 const StyledTableRow = withStyles(theme => ({
   root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.background.default,
-    },
-  },
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.background.default
+    }
+  }
 }))(TableRow);
-
-
 
 const useStyles = makeStyles({
   table: {
-    minWidth: 700,
-  },
+    minWidth: 700
+  }
 });
 function User() {
   const history = useHistory();
 
-   const [userData,setUserData]  =useState([])
+  const [userData, setUserData] = useState([]);
 
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
 
-   useEffect( () => {
-    let data =  localStorage.getItem("userToken")
-   
+    let data = localStorage.getItem("userToken");
+
     let headers = {
       headers: {
         Authorization: `bearer ${data}`
       }
     };
-   
-   
-      axios
-        .get(`http://localhost:3000/users`,headers)
-        .then(response => {
-       
-           setUserData(response.data.data)
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    
-   },[userData.length])
 
+    axios
+      .get(`http://localhost:3000/users`, headers)
+      .then(response => {
+        setUserData(response.data.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, [userData.length]);
 
-
-   if(!localStorage.getItem("userToken")) {
+  if (!localStorage.getItem("userToken")) {
     history.push("/login");
   }
 
-   
-   const classes = useStyles();
+  const classes = useStyles();
   return (
     <Layout>
-     <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="customized table">
-        <TableHead>
-          <TableRow>
-            <StyledTableCell>Email ID</StyledTableCell>
-            <StyledTableCell align="right">Name</StyledTableCell>
-            <StyledTableCell align="right">Mobile</StyledTableCell>
-        
-            <StyledTableCell align="right">Created At</StyledTableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {userData.map(row => (
-            <StyledTableRow key={row._id}>
-              <StyledTableCell component="th" scope="row">
-                {row.email}
-              </StyledTableCell>
-              <StyledTableCell align="right">{row.name}</StyledTableCell>
-              <StyledTableCell align="right">{row.mobile}</StyledTableCell>
-              <StyledTableCell align="right">{row.created_at}</StyledTableCell>
-              
-            </StyledTableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-      </Layout>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Email ID</StyledTableCell>
+              <StyledTableCell align="right">Name</StyledTableCell>
+              <StyledTableCell align="right">Mobile</StyledTableCell>
 
+              <StyledTableCell align="right">Created At</StyledTableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            {loading ? (
+              <div
+                style={{
+                  width: "1200px",
+
+                  justifyContent: "center",
+                  height: "800px",
+                  alignItems: "center"
+                }}
+              >
+                <div>Loading...</div>
+              </div>
+            ) : (
+              userData.map(row => (
+                <StyledTableRow key={row._id}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.email}
+                  </StyledTableCell>
+                  <StyledTableCell align="right">{row.name}</StyledTableCell>
+                  <StyledTableCell align="right">{row.mobile}</StyledTableCell>
+                  <StyledTableCell align="right">
+                    {row.created_at}
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Layout>
   );
 }
 
 export default User;
-
-
-
-
-
-
-
-
